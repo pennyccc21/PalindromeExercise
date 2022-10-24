ae013af191205eeaf83822817b2166274cbddc9a
Peter luque <Peterluque21@gmail.com>
2022-10-23 20:30:13 -0400

    commit

---
+++ PalindromeExercise/PalindromeExercise/Palindrome.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+     public class WordSmith
+    {
+
+        public bool IsPalindrome(string word)
+        {
+            var reversed = "";
+            
+            for (int i = word.Length -1; i > 0; i--)
+            {
+                reversed += word[i];
+            }
+            if(reversed.ToLower() == word.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+            
+    
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,22 @@
 using System;
 using Xunit;
 
-namespace PalindromeExerciseTests
+namespace PalindromeExercise.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar",true)]
+        [InlineData("hello", false)]
+        [InlineData("Racecar", true)]
+        public void MyTest(string word, bool expected)
         {
+            var tester = new WordSmith();
+           
+            
+            bool actual = tester.IsPalindrome(word);
 
+            Assert.Equal(expected, actual);
         }
     }
 }
